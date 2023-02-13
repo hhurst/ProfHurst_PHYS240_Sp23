42558bf8b15c42529a2d0282ee20922b106c6f3f
MatthewHahnGH <matthahn1225@gmail.com>
2023-02-13 15:53:41 -0800

    This is part B for problem 4

--- Hahn/Hahn_Matthew_homework2_problem9.py
+++ Hahn/Hahn_Matthew_homework2_problem9.py
@@ -6,7 +6,7 @@ Created on Mon Feb 13 14:45:08 2023
 """
 import math
 
-print("PART A")
+print("PART B")
 a = float(input("Enter the value for a:"))
 b = float(input("Enter the value for b:"))
 c = float(input("Enter the value for c:"))
@@ -21,4 +21,26 @@ def y():
     return y1
 
 print("The roots for the quadratice equation are:", x(), y())
+
+
+# Creating 2 different equations for the quadratic equation
+def x():
+    x1 = (2 * c) / (0-b - math.sqrt(b**2 - 4 * a * c))
+    return x1
+
+def y():
+    y1 = (2 * c) / (0-b + math.sqrt(b**2 - 4 * a * c))
+    return y1
+
+print("The roots for the quadratice equation are:", x(), y())
+
+# The two equations do not give the same solution for the quadratic equation of the same values for a, b, and c. 
+# I suspect this is because of the amount of bytes that each equations takes up. From the lecture that 
+# Dr. Romanowsky taught, he said that there are only a certain amount of bytes that the computer can store when 
+# using code. I think that the latter version of the quadratic formula uses more bytes than the former resulting 
+# in a different calculation for the values of x.
+
+
+
+
 
